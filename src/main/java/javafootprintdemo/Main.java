f0d98eb55654ae65bd3e0dedfb8470b9ec7f67e0
Manuel <k.song10@163.com>
2020-07-06 08:31:47 +0800

    class field test

--- src/main/java/javafootprintdemo/Main.java
+++ src/main/java/javafootprintdemo/Main.java
@@ -3,7 +3,9 @@ package javafootprintdemo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		while(true) {
+			System.out.println("Hello, world");
+		}
 	}
 
 }
